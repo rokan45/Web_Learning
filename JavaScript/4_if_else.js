d41d68eeebc52db1fe2a949d3cb7f6a4752cddf6
rokan45 <rokanuzzaman4567@gmail.com>
2026-01-24 15:41:54 +0600

    if else conditional statement added

---
+++ JavaScript/4_if_else.js
@@ -0,0 +1,13 @@
+var x=10;
+var y=12;
+
+if(x>y){
+    console.log(" Not true.")
+}
+if (x==y) {
+    console.log("You have learned it")
+    
+} else {
+    console.log("Yeah! x is smaller than y.")
+    
+}
