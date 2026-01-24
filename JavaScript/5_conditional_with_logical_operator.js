22af30eb5ba88a7cd644fe616b3fa573c7f8b25a
rokan45 <rokanuzzaman4567@gmail.com>
2026-01-24 15:49:13 +0600

    logical operator

---
+++ JavaScript/5_conditional_with_logical_operator.js
@@ -0,0 +1,15 @@
+var salary=2000;
+var rent=500;
+var transport=3000;
+
+var Payable= rent<salary;
+var Debt=rent>salary;
+
+if(Payable|| Debt){
+    console.log("You can pay with your money")
+}
+if (rent<2000  && Payable) {
+    console.log("You are capable of paying debt")
+} else {
+    console.log("You have work hard to more income")
+}
