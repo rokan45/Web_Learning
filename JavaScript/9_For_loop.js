2146c0951bb897c898f33b91720aa817e06283e8
rokan45 <rokanuzzaman4567@gmail.com>
2026-01-27 23:15:22 +0600

    for loop basic syntax and even and odd number printing in different way

---
+++ JavaScript/9_For_loop.js
@@ -0,0 +1,19 @@
+//simple for loop. where three parts need to control the loop
+//variable declaration;loop condition;variable changes
+for(let num=0;num<5;num++){
+    console.log(num);
+}
+
+//Printing even number using for loop smaller than 40
+for(let num=0;num<40;num++){
+    if(num%2!==1){
+        console.log(num)
+    }
+}
+
+//Printing odd number less than 40 in desending order
+for(let num=40;num>0;num--){
+    if(num%2!==0){
+        console.log(num)
+    }
+}
