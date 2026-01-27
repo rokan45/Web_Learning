b0832704404bddfd8571e50610207ff27462a897
rokan45 <rokanuzzaman4567@gmail.com>
2026-01-27 22:32:22 +0600

    array basic declaration,lenght,push,pop and includes

---
+++ JavaScript/7_Array.js
@@ -0,0 +1,33 @@
+// Simple array declaration
+const num=[1,34,56,67,8,9,53,90]
+console.log(num)
+
+//lenght of array
+console.log(num.length)
+
+
+
+//Multiple string using array
+const fruits=["mango","banana","apple","berry","jackfruit"]
+console.log(fruits)
+
+//Printing specific array elements using index
+ console.log(fruits[2])
+
+ //Mixed array
+ const mixedArray=[1,"Rokan","cse",true]
+ console.log(mixedArray)
+
+ //Push elements in mixedArray 
+ mixedArray.push("HSTU")
+ console.log(mixedArray)
+
+ //Pop elements from mixedArray
+ mixedArray.pop()
+ console.log(mixedArray)
+
+
+ //Using array for conditional statement
+ if(mixedArray.includes("Rokan")){
+    console.log("Hello! I'm on the list")
+ }
