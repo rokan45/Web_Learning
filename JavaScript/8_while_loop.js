b93429ebcb5de8e2d22919514a9c8e7000559c98
rokan45 <rokanuzzaman4567@gmail.com>
2026-01-27 22:56:13 +0600

    while loop and sum  of 4 iteration of the number 0  to 4

---
+++ JavaScript/8_while_loop.js
@@ -0,0 +1,23 @@
+
+/*const fruits=["mango","banana","apple","berry","jackfruit"]
+console.log(fruits.length)
+
+//Infinite loop using while loop
+
+while(fruits.length>4){
+    console.log(fruits)
+    
+}*/
+
+
+let num=0;
+let sum=0;
+
+//Printing 0 to 4
+while(num<5){
+    console.log(num);
+    sum=sum+num;
+    num++;
+}
+//Sum of the number using loop
+console.log(sum)
