ded55d5411342aeb6f1dc5a17e291682ddb353b7
rokan45 <rokanuzzaman4567@gmail.com>
2026-01-31 22:14:51 +0600

    function basic syntax and function with  parameter

---
+++ JavaScript/12_function.js
@@ -0,0 +1,16 @@
+//basic syntax of function to display some message
+
+function display() {
+    console.log("Tamim and Sakib is dalal of another country");
+    console.log("They only play for money and fame.");
+}
+
+//Call the function
+display();
+
+//function with parameter
+function sqauare(x, y) {
+    return x * y;
+}
+
+console.log(sqauare(4, 5));
