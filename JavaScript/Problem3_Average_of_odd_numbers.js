e81837b86b3f3e402ddaa61563b1355bed71e174
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-02 19:40:15 +0600

    to calculate average of the odd number ,odd number pushing in array named odd

---
+++ JavaScript/Problem3_Average_of_odd_numbers.js
@@ -0,0 +1,19 @@
+//Average of the odd numbers
+function oddAverage(numbers) {
+     const odds = [];
+    for (const number of numbers) {
+       
+        if (number % 2 !== 0) {
+            //console.log(number);
+            odds.push(number);
+
+        }
+    }
+    console.log(odds);
+
+}
+
+const numbers = [12, 21, 34, 35, 67, 11, 33, 45];
+const avg = oddAverage(numbers);
+console.log("The average of the odd number is: ", avg);
+
