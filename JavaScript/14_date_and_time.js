7e105b41903ccf12e3d3e5ae3f0051b311b15d4c
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-02 20:26:22 +0600

    //Lots of method to work with date and time in javaScript and some of them are work like these

---
+++ JavaScript/14_date_and_time.js
@@ -0,0 +1,8 @@
+//Lots of method to work with date and time in javaScript and some of them are work like these
+const today=new Date();
+console.log(today)
+
+const date=new Date('2055-10-5');
+console.log(date)
+console.log(date.getMonth())
+console.log(date.getDay())
