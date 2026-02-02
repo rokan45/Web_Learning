d5d5b187b2c1e295b4b4e2b19da37d3b878f9b5f
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-02 20:15:14 +0600

    There are many build in object to work on different mathematics

--- JavaScript/13_defult_object_in_js.js
+++ JavaScript/13_defult_object_in_js.js
@@ -5,3 +5,15 @@ console.log(min);
 // To find the maximum number
 const max=Math.max(1,23,45,56,77,89);
 console.log(max);
+
+//There are many build in object to work on different mathematics
+console.log(Math.abs(5-10));
+console.log(Math.round(5.38634957934));
+console.log(Math.ceil(5.38634957934));
+console.log(Math.floor(5.38634957934));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.sqrt(25));
+console.log(Math.pow(2,5));
