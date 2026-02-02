ca1943e870d9ea5c1e38dc08dd14f2a1018535fc
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-02 19:23:42 +0600

    accurate logic for the checking leap year

--- JavaScript/Problem2_leapYearcheck.js
+++ JavaScript/Problem2_leapYearcheck.js
@@ -1,5 +1,5 @@
 // Check a year is a leap year or not
-
+//Simple logic
 function checkLeapYear(year){
     if(year%4===0){
         return true;
@@ -10,4 +10,24 @@ function checkLeapYear(year){
 }
 
 const Check=checkLeapYear(2019);
-console.log(Check);
+console.log(Check); //return false
+
+//Accurate Logic
+
+//We know: Those year is not divided by 100 and divisible by 4 is leapyear
+function checkLeapYear(year){
+    if( year%100 !==0 && year%4 ===0){
+        return true;
+    }
+    else if(year%100===0 && year%400 ===0){
+        return true;
+    } 
+    else
+    {
+        return false;
+    }
+       
+}
+
+const Check2=checkLeapYear(2100);
+console.log(Check2); //return false
