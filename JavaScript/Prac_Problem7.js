025929a9286697d90c6ba42d715effb87f028828
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-03 22:59:06 +0600

    Display the physics marks as output.

--- JavaScript/Prac_Problem7.js
+++ JavaScript/Prac_Problem7.js
@@ -24,3 +24,17 @@ car['passenger capcity']=5;
 
 console.log(car);
 
+
+//Display the physics marks as output.
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+console.log(student.physics.marks);
+
