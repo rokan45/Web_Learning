fc3897f2a2ec8f54e226bfb129495b04a1c29693
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-03 20:42:04 +0600

    odd number between 61 to 98 using for nand while loop

---
+++ JavaScript/Prac_Problem5.js
@@ -0,0 +1,15 @@
+//odd number between 61 to 98 by for loop
+
+for(let i=61;i<=98;i+=2){
+    console.log(i);
+}
+
+//odd number between 61 to 98 by while loop
+let num=61;
+while(num<=98){
+    if(num%2==1){
+        console.log(num);
+    }
+    num++;
+    
+}
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+or(let i=61;i<=98;i+=2){
+//     console.log(i);
+// }
