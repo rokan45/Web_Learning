b827be39454ea16b33d528406c3800b8729e5186
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-03 23:39:40 +0600

    all type of object syntax have to know

---
+++ JavaScript/17_Object.js
@@ -0,0 +1,64 @@
+//Access the golden rod color value in output.
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+console.log(colors["golden rod"]);
+
+//To acess the object element diffrent way
+console.log(colors.red);
+console.log(colors['blue']);
+
+//For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car['passenger capcity'] = 5;
+
+console.log(car);
+
+
+//Display the physics marks as output.
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+console.log(student.physics.marks);
+
+//Count the number of properties.
+let student2 = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+const counter = Object.keys(student2).length;
+console.log(counter);
+
+//loop through an object and print the key-value pairs with their types
+
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+}
+
+for(const prop in myObject){
+   
+    console.log("key: "+prop," | "+"type: "+typeof(myObject[prop]));
+}
