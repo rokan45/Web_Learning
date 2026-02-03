3db623ace58e9a228b773264cb1d8d479c2c868b
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-04 00:07:24 +0600

    using loop  display the even number form an array

--- JavaScript/Prac_Problem8.js
+++ JavaScript/Prac_Problem8.js
@@ -11,3 +11,13 @@ for(const color of colors)
 }
 
 console.log(newReverse);
+
+//Write a JavaScript code to get the even numbers from an array using any looping technique.
+
+const randomNUM = [12, 98, 5, 41, 23, 78, 46];
+
+for(const num of randomNUM){
+    if(num%2===0){
+        console.log(num);
+    }
+}
--- JavaScript/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-or(let i=61;i<=98;i+=2){
-//     console.log(i);
-// }
