c015c491f55e5ddf54f1188d87e9caeca593b242
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-05 21:16:19 +0600

    copy the orinal array and change the first element

--- JavaScript/Prac_Problem9.js
+++ JavaScript/Prac_Problem9.js
@@ -5,3 +5,17 @@ const statement = 'I am a hard working person'
 const reversed = statement.split(" ").reverse().join(" ");
 
 console.log(reversed);
+
+//Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+
+const number = [1, 2, 3];
+console.log("Original:",number);
+const copy = [];
+
+
+for (const i of number) {
+    copy.push(i);
+}
+
+copy[0] = 99;
+console.log("Copy:",copy);
