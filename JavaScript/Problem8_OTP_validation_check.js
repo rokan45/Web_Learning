5981762f1e54c91c438efb253a8fcc22b4030870
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-06 20:58:19 +0600

    cheking string and length

--- JavaScript/Problem8_OTP_validation_check.js
+++ JavaScript/Problem8_OTP_validation_check.js
@@ -1,4 +1,14 @@
 function validOtp(otp) {
-    // Your code here
-    
+    if (typeof otp === "string" && otp.length == 8 && otp.startsWith("ph", 0)) {
+        return true;
+
+    }
+    else if(typeof otp !=="string"){
+        return "Invalid";
+
+    }
+    else {
+        return false;
+    }
 }
+console.log(validOtp("ph-10985"));
