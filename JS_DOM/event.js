33acee1c6f41cdff05ce71f4bc5359f810cadcdf
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-17 23:01:53 +0600

    multiple way to use event handle

---
+++ JS_DOM/event.js
@@ -0,0 +1,15 @@
+//Red button function
+function makeRed(){
+            document.body.style.backgroundColor='red';
+        }
+
+
+
+// Black button function
+const makeblack=document.getElementById('btn-black');
+makeblack.onclick=makeBlack;
+
+
+function makeBlack(){
+            document.body.style.backgroundColor='black';
+        }
