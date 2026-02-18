e9e7bd4b11e6f9851957d183f00501511772e097
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-18 10:43:05 +0600

    event handler to change text from a input field

---
+++ JS_DOM/handler.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-text').addEventListener('click',function(){
+            // console.log("Clicked!")
+            const changeTitle=document.getElementById('title');
+            changeTitle.innerText='Fuck Hstu';
+
+        })
