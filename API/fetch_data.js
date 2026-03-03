a3418960bf0e99976fa65e055024fd1cf07abf64
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-03 14:58:06 +0600

    fetch 100 post data and display the data into a container in list item with post title only

--- API/fetch_data.js
+++ API/fetch_data.js
@@ -3,4 +3,34 @@ const load = () => {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(mydata => mydata.json())
         .then(json => console.log(json))
+};
+
+
+//function and fetch data for loadPost
+
+const loadPost = () => {
+    const link = "https://jsonplaceholder.typicode.com/posts";
+
+    //fetch data from link
+    fetch(link)
+        .then(linkData => linkData.json())
+        .then(json => displayPost(json))  //pass to another function
+};
+
+//display post from this function
+const displayPost = (posts) => {
+    //1. get the element
+    const postConatiner = document.getElementById("post-container");
+
+    // we can display each post itereating usin forEach function
+    posts.forEach(post => {
+
+        //2.create element and get post title
+        const li = document.createElement("li");
+        li.innerText = post.title;
+
+        //3.append with parent element
+        postConatiner.appendChild(li);
+
+    });
 }
