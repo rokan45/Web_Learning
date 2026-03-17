498ee557a36b9526d527745a2951e6ca6e8d6f63
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-17 23:09:20 +0600

    exploring react basic concept from another project

---
+++ basic-react-II/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+
+        <div>
+          <h1>Get started</h1>
+        </div>
+
+      </section>
+
+    </>
+  )
+}
+
+export default App
