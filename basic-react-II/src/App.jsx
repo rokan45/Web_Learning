5fa4f787100f422cca9c6c7be2e85f392c91cc26
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-19 00:40:55 +0600

    react concept useState to count

--- basic-react-II/src/App.jsx
+++ basic-react-II/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Counter from './counter'
 
 function App() {
   function HandleClick() {
@@ -11,7 +12,16 @@ function App() {
 
   return (
     <>
-      <section> <h1>React Core Concept</h1></section>
+      <section className='basic'> 
+        {/* style can be applied dynamically */}
+        <h1 style={{ textAlign: 'center' }}>React Core Concept</h1>
+      </section>
+
+      <section>
+        <Counter></Counter>
+      </section>
+
+
       <section>
         {/* //One way to handle click event */}
         <button onClick={HandleClick}>Click Here</button>
@@ -25,7 +35,7 @@ function App() {
         }}>On click</button>
 
         {/* //4th way direct on arrow function */}
-        <button onClick={()=>alert("Using asrrow")}>Using arrow</button>
+        <button onClick={() => alert("Using asrrow")}>Using arrow</button>
       </section>
 
     </>
---
+++ basic-react-II/src/counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const handleCount=()=>{
+        // setCount(1);
+        const newCount=count+1;
+        setCount(newCount);
+    }
+
+    //style for the componet
+    const counterStyle = {
+        border: '2px solid green',
+        padding: '10px',
+    }
+    return (
+        <div style={counterStyle}>
+            <h3>Count: {count} </h3>
+            <button onClick={handleCount}>Add</button>
+        </div>
+    )
+}
