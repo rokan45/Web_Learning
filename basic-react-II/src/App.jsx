8c00121a0681eb7c00d45af51e8cc7fe706dd48d
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-18 22:32:50 +0600

    diffrent way to handle on click event

--- basic-react-II/src/App.jsx
+++ basic-react-II/src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function HandleClick() {
+    alert("Clicked!")
+  }
+  const Add5 = (Number) => {
+    alert(Number + 5)
+  }
 
   return (
     <>
-      <section id="center">
+      <section> <h1>React Core Concept</h1></section>
+      <section>
+        {/* //One way to handle click event */}
+        <button onClick={HandleClick}>Click Here</button>
 
-        <div>
-          <h1>Get started</h1>
-        </div>
+        {/* //2nd way to handle Click: Passing argument */}
+        <button onClick={() => Add5(10)}>Add 5</button>
 
+        {/* //3rd way to handle click: Not usual practice */}
+        <button onClick={function ClickIt() {
+          alert("Yeah click me!")
+        }}>On click</button>
+
+        {/* //4th way direct on arrow function */}
+        <button onClick={()=>alert("Using asrrow")}>Using arrow</button>
       </section>
 
     </>
