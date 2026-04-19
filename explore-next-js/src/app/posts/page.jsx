d258ed0667719246d4a1a7e7b90a530f82735bbf
rokan45 <rokanuzzaman4567@gmail.com>
2026-04-20 02:13:49 +0600

    data fetching and create json server

--- explore-next-js/src/app/posts/page.jsx
+++ explore-next-js/src/app/posts/page.jsx
@@ -1,9 +1,23 @@
 
 
-const PostPage = () => {
+const PostPage = async () => {
+
+    // 1.
+    // const postData = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const data=await postData.json();
+    // console.log(data)
+
+
+    // 2.
+    const getPost=async()=>{
+        const res= await fetch('https://jsonplaceholder.typicode.com/posts');
+        return res.json();
+    }
+
+
     return (
         <div>
-            <h1>Hey Im here!</h1>
+            <h1>Hey Im here {data.length}!</h1>
         </div>
     );
 };
